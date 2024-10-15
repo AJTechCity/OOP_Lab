4ac365a7676aa22a469add3962968018a5a13f86
AJ Tech City <arunjai12456@icloud.com>
2024-10-15 18:34:15 +0100

    Finished Derviation Task 2

--- Week 2/Challenge/Derivations.java
+++ Week 2/Challenge/Derivations.java
@@ -12,5 +12,16 @@ public class Derivations{
         System.out.println(derivation1);
 
         /* Solution is to apply rules in the order 2,3,4,3,4,1 */
+
+        /* Problem 2, Derive MUIIU from MII */
+        Expression derivation2 = new Expression("MII");
+        derivation2.ruleTwo();
+        derivation2.ruleThree();
+        derivation2.ruleOne();
+        derivation2.ruleTwo();
+        derivation2.ruleFour();
+        System.out.println(derivation2);
+
+        /* Solution is to apply rules in the order 2,3,1,2,4 */
     }
 }
