acb43edc0f7ca0ff4dfc9b6ae26ec00534358581
AJ Tech City <arunjai12456@icloud.com>
2024-10-29 23:13:50 +0000

    Room.java filled

--- Assignment 1/src/org/uob/a1/Room.java
+++ Assignment 1/src/org/uob/a1/Room.java
@@ -1,5 +1,28 @@
 package org.uob.a1;
 
 public class Room {
-    
+
+    private String name;
+    private String description;
+    private char symbol;
+    private Position position;
+
+    public Room(String name, String description, char symbol, Position position){
+        this.name = name;
+        this.description = description;
+        this.symbol = symbol;
+        this.position = position;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public char getSymbol(){
+        return this.symbol;
+    }
+
+    public Position getPosition(){
+        return this.position;
+    }
 }
