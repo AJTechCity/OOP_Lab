73e68ee6b8feae86cf07160126c25341c3c17ada
AJ Tech City <arunjai12456@icloud.com>
2024-10-30 00:18:57 +0000

    General Updates

--- Assignment 1/src/org/uob/a1/Game.java
+++ Assignment 1/src/org/uob/a1/Game.java
@@ -6,9 +6,21 @@ public class Game {
     
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        Score s = new Score(0);
+        Position userPosition = new Position(0,0);
+
+        do{
+
+        }while(command != "quit");
+
+        System.out.println("Game over - user quite the experience");
+        System.out.println("Final score was: " + s.getScore());
+
         Map gameMap = new Map(10,10);
 
-        Room[] rooms = { 
+        Room[] rooms = { //Create array with all rooms within it
             new Room("Room A", "A dusty, dirty old room", 'A', new Position(1,2)),
             new Room("Room B", "A dusty, dirty old room", 'B', new Position(4,1)),
             new Room("Room C", "A dusty, dirty old room", 'C', new Position(1,3)),
@@ -16,7 +28,7 @@ public class Game {
             new Room("Room E", "A dusty, dirty old room", 'E', new Position(2,6)),
         };
 
-        for(int i=0;i<rooms.length;i++){
+        for(int i=0;i<rooms.length;i++){ //Loop over rooms array so that they can be added to map programatically
             Room r = rooms[i];
             gameMap.placeRoom(r.getPosition(), r.getSymbol());
         }
--- Week 3/Week3.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Week3{
-    public static void main(String[] args){
-        Day d = new Day(1,1,1900);
-        System.out.println(d.dayOfWeek());
-        
-    }
-}
