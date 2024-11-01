d83cb10439c300989d7d4c09e2777453566976c0
AJ Tech City <arunjai12456@icloud.com>
2024-11-01 23:18:57 +0000

    Fix

---
+++ Week 2/Challenge/Test.java
@@ -0,0 +1,33 @@
+public class Test{
+    public static void main(String[] args){
+        /* Tests to test Rule 1*/
+
+        Expression test1 = new Expression("MI");
+        System.out.println(test1); //Should output MI
+        test1.ruleOne();
+        System.out.println(test1); //Should output MIU
+
+        Expression test2 = new Expression("MUU");
+        System.out.println(test2); //Should output MUU
+        test2.ruleOne();
+        System.out.println(test2); //Should output MUU
+
+        /*Tests to test Rule 2*/
+        Expression test3 = new Expression("MIU");
+        System.out.println(test3); //Should output MIU
+        test3.ruleTwo();
+        System.out.println(test3); //Should output MIUIU
+
+        /*Tests to test Rule 3*/
+        Expression test4 = new Expression("MUIIIU");
+        System.out.println(test4); //Should output MUIIIU
+        test4.ruleThree();
+        System.out.println(test4); //Should output MUUU
+
+        /*Tests to test Rule 4*/
+        Expression test5 = new Expression("MUUU");
+        System.out.println(test5); //Should output MUUU
+        test5.ruleFour();
+        System.out.println(test5); //Should output Mu
+    }
+}
